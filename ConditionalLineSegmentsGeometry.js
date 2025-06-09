ed8fc0ad556ee8f7e50b3ecadb04803dfac4d076
Deepak Suthar <dpkstr@hotmail.com>
2025-06-09 19:02:33 +0530

    Update ConditionalLineSegmentsGeometry.js

--- ConditionalLineSegmentsGeometry.js
+++ ConditionalLineSegmentsGeometry.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.154.0/build/three.module.js';
-import { LineSegmentsGeometry } from '//cdn.skypack.dev/three@0.130.1/examples/jsm/lines/LineSegmentsGeometry.js';
+import { LineSegmentsGeometry } from 'https://cdn.jsdelivr.net/npm/three@0.154.0/examples/jsm/lines/LineSegmentsGeometry.js';
 
 export class ConditionalLineSegmentsGeometry extends LineSegmentsGeometry {
 
